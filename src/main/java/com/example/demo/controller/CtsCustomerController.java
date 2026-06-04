0d54b39cd8d20333739e1b355aa137ad5a9c9d1e
sisupe <admin@sisupe.com>
2026-06-05 00:54:16 +0530

    learning about git command

--- src/main/java/com/example/demo/controller/CtsCustomerController.java
+++ src/main/java/com/example/demo/controller/CtsCustomerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.dto.CustomerDto;
 import com.example.demo.dto.EmployeeDto;
 import com.example.demo.service.CtsCustomerService;
-
+//customer controller
 //multiple primary key
 @RestController
 @RequestMapping("/customer")
--- src/main/java/com/example/demo/controller/CtsException.java
+++ src/main/java/com/example/demo/controller/CtsException.java
@@ -1,5 +1,5 @@
 package com.example.demo.controller;
-
+//exception
 public class CtsException extends Exception {
 
 	public CtsException(String message) {
